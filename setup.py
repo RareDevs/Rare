7b481da8db8baee732c908c0034b4d728973268d
Dummerle <lennardbaeumer@gmail.com>
2021-01-06 21:03:24 +0100

    setup.py bugfix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Dummerle/Rare",
-    packages=setuptools.find_packages(where="Rare/"),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
