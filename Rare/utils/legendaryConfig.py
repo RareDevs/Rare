80dfb2a87bd7691867cbf43c7ea03fbd2b6515c2
Dummerle <lennardbaeumer@gmail.com>
2021-01-11 09:05:04 +0100

    Fixed lowercase error

--- Rare/utils/legendaryConfig.py
+++ Rare/utils/legendaryConfig.py
@@ -3,14 +3,15 @@ import os
 
 config_path = os.path.expanduser("~") + "/.config/legendary/"
 lgd_config = configparser.ConfigParser()
-
+lgd_config.optionxform = str
 
 def get_config() -> {}:
-    lgd_config.read(config_path + "config.ini")
 
+    lgd_config.read(config_path + "config.ini")
+    lgd_config.optionxform = str
     return lgd_config
 
 
-def set_config( new_config: {}):
+def set_config(new_config: {}):
     lgd_config = new_config
     lgd_config.write(open(config_path + "config.ini", "w"))
