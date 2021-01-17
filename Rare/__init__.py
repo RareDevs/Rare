a47cc114b6b3d88b835d6b965ecd7847ffab8506
Dummerle <lennardbaeumer@gmail.com>
2021-01-17 17:57:17 +0100

    Small Fixes

--- Rare/__init__.py
+++ Rare/__init__.py
@@ -1,4 +1,4 @@
 import os
-__version__ = "0.1.5"
+__version__ = "0.2.0"
 style_path = os.path.join(os.path.dirname(__file__), "Styles/")
 lang_path = os.path.join(os.path.dirname(__file__), "languages/")
