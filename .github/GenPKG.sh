9f4759b534d20b93dbfc97c1d9eff215163decc2
Dummerle <44114474+Dummerle@users.noreply.github.com>
2021-03-17 14:25:24 +0100

    Update GenPKG.sh

--- .github/GenPKG.sh
+++ .github/GenPKG.sh
@@ -1,4 +1,6 @@
 export PYTHONPATH=$PWD
 version=$(python3 Rare --version)
-
+cd .github
+git clone https://aur.archlinux.org/rare.git
+cd ..
 sed -i "s/.*pkgver=.*/pkgver=$version/" .github/rare/PKGBUILD
