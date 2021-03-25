b916b044ade29603c98d5ffd3d94913ee6fef995
Dummerle <lennardbaeumer@gmail.com>
2021-03-25 14:39:13 +0100

    Change version

--- Rare/__init__.py
+++ Rare/__init__.py
@@ -1,5 +1,5 @@
 import os
 
-__version__ = "0.9.6"
+__version__ = "0.9.7"
 style_path = os.path.join(os.path.dirname(__file__), "Styles/")
 lang_path = os.path.join(os.path.dirname(__file__), "languages/")
