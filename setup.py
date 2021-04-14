0b31fe9ee23ff85e3748c391487cf882f97de10b
ChemicalXandco <32775248+ChemicalXandco@users.noreply.github.com>
2021-04-14 21:50:38 +0100

    linux support for new build system

--- setup.py
+++ setup.py
@@ -4,33 +4,6 @@ from cx_Freeze import setup, Executable
 
 import rare
 
-base = None
-name = None
-shortcutName = None
-shortcutDir = None
-bdist_msi_options = None
-if sys.platform == 'win32':
-    base = 'Win32GUI'
-    name = 'Rare.exe'
-    shortcut_table = [
-        ('DesktopShortcut',        # Shortcut
-         'DesktopFolder',          # Directory
-         'Rare',                   # Name
-         'TARGETDIR',              # Component
-         '[TARGETDIR]'+name,       # Target
-         None,                     # Arguments
-         'A gui for Legendary.',   # Description
-         None,                     # Hotkey
-         None,                     # Icon
-         None,                     # IconIndex
-         None,                     # ShowCmd
-         'TARGETDIR'               # Working Directory
-         )]
-    msi_data = {"Shortcut": shortcut_table}
-    bdist_msi_options = {'data': msi_data}
-else:
-    name = 'Rare'
-
 opts = {
     'packages': [
         'requests',
@@ -52,10 +25,43 @@ opts = {
     ],
 }
 
+setup_options = {'build_exe': opts}
+base = None
+name = None
+shortcutName = None
+shortcutDir = None
+bdist_msi_options = None
+
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+    name = 'Rare.exe'
+    shortcut_table = [
+        ('DesktopShortcut',        # Shortcut
+         'DesktopFolder',          # Directory
+         'Rare',                   # Name
+         'TARGETDIR',              # Component
+         '[TARGETDIR]'+name,       # Target
+         None,                     # Arguments
+         'A gui for Legendary.',   # Description
+         None,                     # Hotkey
+         None,                     # Icon
+         None,                     # IconIndex
+         None,                     # ShowCmd
+         'TARGETDIR'               # Working Directory
+         )]
+    msi_data = {"Shortcut": shortcut_table}
+    bdist_msi_options = {'data': msi_data}
+    setup_options.update({"bdist_msi": bdist_msi_options})
+else:
+    name = 'Rare'
+
 setup(name = 'Rare',
     version = rare.__version__,
     description = 'A gui for Legendary.',
-    options = {'build_exe': opts, "bdist_msi": bdist_msi_options},
+    options = setup_options,
+    setup_requires=[
+        'cx_Freeze',
+    ],
     executables = [
         Executable('rare/__main__.py',
             targetName=name,
