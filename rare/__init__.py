1123757099b44d48b64aa5fc453c28a6414a386f
Dummerle <lennardbaeumer@gmail.com>
2021-04-17 12:09:21 +0200

    Prepare 1.2.0

--- rare/__init__.py
+++ rare/__init__.py
@@ -1,5 +1,5 @@
 import os
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 style_path = os.path.join(os.path.dirname(__file__), "styles/")
 lang_path = os.path.join(os.path.dirname(__file__), "languages/")
