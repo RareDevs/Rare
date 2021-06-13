c077e57a26b27befb80f36284b8694e3412e8b27
Dummerle <lennardbaeumer@gmail.com>
2021-06-13 15:06:29 +0200

    Read games from api

--- rare/components/tabs/shop/shop_widget.py
+++ rare/components/tabs/shop/shop_widget.py
@@ -63,8 +63,37 @@ class ShopWidget(QScrollArea, Ui_ShopWidget):
 
         game_list = ["Satisfactory", "Among Us", "Star Wars Jedi Fallen Order", "Watch Dogs", "Subnautica Below Zero"]
         # TODO read from api
-        for game in game_list:
-            w = GameWidget.from_request(game, self.path)
+        locale = get_lang()
+        payload = json.dumps(
+            {"variables": {"category": "games/edition/base|bundles/games|editors|software/edition/base",
+                           "count": 15, "country": locale.upper(), "keywords": "", "locale": locale,
+                           "sortDir": "DESC", "allowCountries": locale.upper(), "start": 0, "tag": "",
+                           "withMapping": True, "withPrice": True
+                           },
+             "query": game_query}).encode()
+
+        request = QNetworkRequest(QUrl("https://www.epicgames.com/graphql"))
+        request.setHeader(QNetworkRequest.ContentTypeHeader, "application/json")
+        self.game_request = self.manager.post(request, payload)
+        self.game_request.finished.connect(self.show_recommended_games)
+
+    def show_recommended_games(self):
+        print("lol")
+        if self.game_request:
+            if self.game_request.error() == QNetworkReply.NoError:
+                try:
+                    games = json.loads(self.game_request.readAll().data().decode())["data"]["Catalog"]["searchStore"][
+                        "elements"]
+                    print(games)
+                except JSONDecodeError:
+                    return
+            else:
+                return
+        else:
+            return
+
+        for game in games:
+            w = GameWidget(self.path, game)
             self.games_groupbox.layout().addWidget(w)
             w.show_info.connect(self.show_game.emit)
 
@@ -333,3 +362,35 @@ query = "query searchStoreQuery($allowCountries: String, $category: String, $cou
         "endDate\n              discountSetting {\n                discountType\n                discountPercentage\n " \
         "             }\n            }\n          }\n        }\n      }\n      paging {\n        count\n        " \
         "total\n      }\n    }\n  }\n}\n "
+
+game_query = "query searchStoreQuery($allowCountries: String, $category: String, $count: Int, $country: String!, " \
+             "$keywords: String, $locale: String, $namespace: String, $withMapping: Boolean = false, $itemNs: String, " \
+             "$sortBy: String, $sortDir: String, $start: Int, $tag: String, $releaseDate: String, $withPrice: Boolean " \
+             "= false, $withPromotions: Boolean = false, $priceRange: String, $freeGame: Boolean, $onSale: Boolean, " \
+             "$effectiveDate: String) {\n  Catalog {\n    searchStore(\n      allowCountries: $allowCountries\n      " \
+             "category: $category\n      count: $count\n      country: $country\n      keywords: $keywords\n      " \
+             "locale: $locale\n      namespace: $namespace\n      itemNs: $itemNs\n      sortBy: $sortBy\n      " \
+             "sortDir: $sortDir\n      releaseDate: $releaseDate\n      start: $start\n      tag: $tag\n      " \
+             "priceRange: $priceRange\n      freeGame: $freeGame\n      onSale: $onSale\n      effectiveDate: " \
+             "$effectiveDate\n    ) {\n      elements {\n        title\n        id\n        namespace\n        " \
+             "description\n        effectiveDate\n        keyImages {\n          type\n          url\n        }\n     " \
+             "   currentPrice\n        seller {\n          id\n          name\n        }\n        productSlug\n       " \
+             " urlSlug\n        url\n        tags {\n          id\n        }\n        items {\n          id\n         " \
+             " namespace\n        }\n        customAttributes {\n          key\n          value\n        }\n        " \
+             "categories {\n          path\n        }\n        catalogNs @include(if: $withMapping) {\n          " \
+             "mappings(pageType: \"productHome\") {\n            pageSlug\n            pageType\n          }\n        " \
+             "}\n        offerMappings @include(if: $withMapping) {\n          pageSlug\n          pageType\n        " \
+             "}\n        price(country: $country) @include(if: $withPrice) {\n          totalPrice {\n            " \
+             "discountPrice\n            originalPrice\n            voucherDiscount\n            discount\n           " \
+             " currencyCode\n            currencyInfo {\n              decimals\n            }\n            fmtPrice(" \
+             "locale: $locale) {\n              originalPrice\n              discountPrice\n              " \
+             "intermediatePrice\n            }\n          }\n          lineOffers {\n            appliedRules {\n     " \
+             "         id\n              endDate\n              discountSetting {\n                discountType\n     " \
+             "         }\n            }\n          }\n        }\n        promotions(category: $category) @include(if: " \
+             "$withPromotions) {\n          promotionalOffers {\n            promotionalOffers {\n              " \
+             "startDate\n              endDate\n              discountSetting {\n                discountType\n       " \
+             "         discountPercentage\n              }\n            }\n          }\n          " \
+             "upcomingPromotionalOffers {\n            promotionalOffers {\n              startDate\n              " \
+             "endDate\n              discountSetting {\n                discountType\n                " \
+             "discountPercentage\n              }\n            }\n          }\n        }\n      }\n      paging {\n   " \
+             "     count\n        total\n      }\n    }\n  }\n}\n "
