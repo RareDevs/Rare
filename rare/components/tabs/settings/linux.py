c7e5675c8968b493342d1b90ebc950778f8c7367
Dummerle <44114474+Dummerle@users.noreply.github.com>
2021-11-10 19:00:15 +0100

    Update savepath on button click

--- rare/components/tabs/settings/linux.py
+++ rare/components/tabs/settings/linux.py
@@ -1,7 +1,6 @@
-import os
 from logging import getLogger
 
-from PyQt5.QtWidgets import QFileDialog, QWidget, QMessageBox
+from PyQt5.QtWidgets import QFileDialog, QWidget
 
 from rare import shared
 from rare.components.tabs.settings.dxvk import DxvkSettings
@@ -48,19 +47,3 @@ class LinuxSettings(QWidget, Ui_LinuxSettings):
         if self.core.lgd.config[self.name] == {}:
             self.core.lgd.config.remove_section(self.name)
         self.core.lgd.save_config()
-
-        if setting_name == "wine_prefix":
-            if not os.path.isdir(text) or not os.path.exists(os.path.join(text, "user.reg")):
-                return
-            if self.name != "default":  # if game
-                game = self.core.get_game(self.name)
-                if self.core.is_installed(self.name) and game.supports_cloud_saves:
-                    try:
-                        new_path = self.core.get_save_path(self.name)
-                    except Exception as e:
-                        QMessageBox.warning(self, "Error", self.tr("Could not compute save path for {}").format(
-                            game.app_title) + "\n" + str(e))
-                        return
-                    igame = self.core.get_installed_game(self.name)
-                    igame.save_path = new_path
-                    self.core.lgd.set_installed_game(self.name, igame)
