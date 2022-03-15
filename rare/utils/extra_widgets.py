3c148d8093ef527bf83a8363b95259f5f6518d43
Dummerle <44114474+Dummerle@users.noreply.github.com>
2022-03-15 20:34:39 +0100

    Add wrapper for PahEdit edit function to accept "~" as $HOME

--- rare/utils/extra_widgets.py
+++ rare/utils/extra_widgets.py
@@ -318,6 +318,9 @@ class PathEdit(IndicatorLineEdit):
         self.compl_model.setIconProvider(PathEditIconProvider())
         self.compl_model.setRootPath(path)
         self.completer.setModel(self.compl_model)
+
+        edit_func = self._wrap_edit_function(edit_func)
+
         super(PathEdit, self).__init__(
             text=path,
             placeholder=placeholder,
@@ -356,6 +359,13 @@ class PathEdit(IndicatorLineEdit):
             names = dlg.selectedFiles()
             self.line_edit.setText(names[0])
             self.compl_model.setRootPath(names[0])
+
+    def _wrap_edit_function(self, edit_function: Callable[[str], Tuple[bool, str, str]]):
+        if edit_function:
+            return lambda text: edit_function(os.path.expanduser(text)
+                                              if text.startswith("~") else text)
+        else:
+            return edit_function
 
 
 class SideTabBar(QTabBar):
