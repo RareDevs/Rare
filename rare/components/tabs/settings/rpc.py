64c9ecbed9cca6e94bb260c2db1636dd1456d5c1
Dummerle <44114474+Dummerle@users.noreply.github.com>
2022-03-16 18:27:03 +0100

    Fix some rebase errors

--- rare/components/tabs/settings/rpc.py
+++ rare/components/tabs/settings/rpc.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import QSettings
 from PyQt5.QtWidgets import QGroupBox
 
-from rare.shared import GlobalSignalsSingleton
+import rare.shared as shared
 from rare.ui.components.tabs.settings.rpc import Ui_RPCSettings
 
 
@@ -9,7 +9,7 @@ class RPCSettings(QGroupBox, Ui_RPCSettings):
     def __init__(self, parent):
         super(RPCSettings, self).__init__(parent=parent)
         self.setupUi(self)
-        self.signals = GlobalSignalsSingleton()
+        self.signals = shared.signals
 
         self.settings = QSettings()
 
