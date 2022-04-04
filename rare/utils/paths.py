5b8bb411ee55064333e39eb50eb955b93e047c51
Dummerle <44114474+Dummerle@users.noreply.github.com>
2022-04-04 23:21:23 +0200

    Don't create resources folder in paths.py

--- rare/utils/paths.py
+++ rare/utils/paths.py
@@ -6,13 +6,12 @@ code_name = "Stellula Kakopo"
 
 from PyQt5.QtCore import QStandardPaths
 
-
-resources_path = os.path.join(os.path.dirname(__file__), "../resources")
+resources_path = os.path.join(os.path.dirname(__file__), "../resources/")
 data_dir = os.path.join(QStandardPaths.writableLocation(QStandardPaths.DataLocation), "rare")
 cache_dir = os.path.join(QStandardPaths.writableLocation(QStandardPaths.CacheLocation), "rare")
 image_dir = os.path.join(data_dir, "images")
 tmp_dir = os.path.join(cache_dir, "tmp")
 
-for path in (resources_path, data_dir, cache_dir, image_dir, tmp_dir):
+for path in (data_dir, cache_dir, image_dir, tmp_dir):
     if not os.path.exists(path):
         os.makedirs(path)
