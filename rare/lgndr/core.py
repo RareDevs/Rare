a9954a98f6ae217ca380b5d8abd554ce4ac6cf3b
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2022-06-25 19:44:35 +0300

    LegendaryCore: Re-use super's `prepare_download()` instead of copying it.

--- rare/lgndr/core.py
+++ rare/lgndr/core.py
@@ -3,12 +3,10 @@ import os
 from multiprocessing import Queue
 from typing import Callable
 
-from legendary.utils.lfs import clean_filename, validate_files
+from legendary.utils.lfs import validate_files
 from legendary.models.downloading import AnalysisResult
 from legendary.models.game import *
-from legendary.utils.game_workarounds import is_opt_enabled
 from legendary.utils.selective_dl import get_sdl_appname
-from legendary.utils.manifests import combine_manifests
 
 from legendary.core import LegendaryCore as LegendaryCoreReal
 from .manager import DLManager
@@ -16,7 +14,7 @@ from .manager import DLManager
 
 class LegendaryCore(LegendaryCoreReal):
 
-    def prepare_download(self, app_name: str, base_path: str = '', no_install: bool = False,
+    def prepare_download(self, app_name: str, base_path: str = '',
                          status_q: Queue = None, max_shm: int = 0, max_workers: int = 0,
                          force: bool = False, disable_patching: bool = False,
                          game_folder: str = '', override_manifest: str = '',
@@ -27,9 +25,10 @@ class LegendaryCore(LegendaryCoreReal):
                          repair: bool = False, repair_use_latest: bool = False,
                          disable_delta: bool = False, override_delta_manifest: str = '',
                          egl_guid: str = '', preferred_cdn: str = None,
-                         disable_https: bool = False, ignore_space_req: bool = False, reset_sdl: bool = False,
-                         sdl_prompt: Callable[[str, str], List[str]] = list) \
-            -> (DLManager, AnalysisResult, Game, InstalledGame):
+                         no_install: bool = False, ignore_space_req: bool = False,
+                         disable_sdl: bool = False, reset_sdl: bool = False, skip_sdl: bool = False,
+                         sdl_prompt: Callable[[str, str], List[str]] = list,
+                         disable_https: bool = False) -> (DLManager, AnalysisResult, InstalledGame, Game):
         if self.is_installed(app_name):
             igame = self.get_installed_game(app_name)
             platform = igame.platform
@@ -37,7 +36,7 @@ class LegendaryCore(LegendaryCoreReal):
                 self.log.info('Game needs to be verified before updating, switching to repair mode...')
                 repair = True
 
-        repair_file = ''
+        repair_file = None
         if repair:
             repair = True
             no_install = repair_use_latest is False
@@ -77,221 +76,84 @@ class LegendaryCore(LegendaryCoreReal):
             else:
                 self.log.info(f'Using existing repair file: {repair_file}')
 
-        # Workaround for Cyberpunk 2077 preload
-        if not file_install_tag and not game.is_dlc and ((sdl_name := get_sdl_appname(game.app_name)) is not None):
-            config_tags = self.lgd.config.get(game.app_name, 'install_tags', fallback=None)
+        # check if SDL should be disabled
+        sdl_enabled = not file_install_tag and not game.is_dlc
+        config_tags = self.lgd.config.get(game.app_name, 'install_tags', fallback=None)
+        config_disable_sdl = self.lgd.config.getboolean(game.app_name, 'disable_sdl', fallback=False)
+        # remove config flag if SDL is reset
+        if config_disable_sdl and reset_sdl and not disable_sdl:
+            self.lgd.config.remove_option(game.app_name, 'disable_sdl')
+        # if config flag is not yet set, set it and remove previous install tags
+        elif not config_disable_sdl and disable_sdl:
+            self.log.info('Clearing install tags from config and disabling SDL for title.')
+            if config_tags:
+                self.lgd.config.remove_option(game.app_name, 'install_tags')
+                config_tags = None
+            self.lgd.config.set(game.app_name, 'disable_sdl', True)
+            sdl_enabled = False
+        # just disable SDL, but keep config tags that have been manually specified
+        elif config_disable_sdl or disable_sdl:
+            sdl_enabled = False
+
+        if sdl_enabled and ((sdl_name := get_sdl_appname(game.app_name)) is not None):
             if not self.is_installed(game.app_name) or config_tags is None or reset_sdl:
-                file_install_tag = sdl_prompt(sdl_name, game.app_title)
-                if game.app_name not in self.lgd.config:
-                    self.lgd.config[game.app_name] = dict()
-                self.lgd.config.set(game.app_name, 'install_tags', ','.join(file_install_tag))
+                sdl_data = self.get_sdl_data(sdl_name, platform=platform)
+                if sdl_data:
+                    if skip_sdl:
+                        file_install_tag = ['']
+                        if '__required' in sdl_data:
+                            file_install_tag.extend(sdl_data['__required']['tags'])
+                    else:
+                        file_install_tag = sdl_prompt(sdl_data, game.app_title)
+                    self.lgd.config.set(game.app_name, 'install_tags', ','.join(file_install_tag))
+                else:
+                    self.log.error(f'Unable to get SDL data for {sdl_name}')
             else:
                 file_install_tag = config_tags.split(',')
+        elif file_install_tag and not game.is_dlc and not no_install:
+            config_tags = ','.join(file_install_tag)
+            self.log.info(f'Saving install tags for "{game.app_name}" to config: {config_tags}')
+            self.lgd.config.set(game.app_name, 'install_tags', config_tags)
+        elif not game.is_dlc:
+            if config_tags and reset_sdl:
+                self.log.info('Clearing install tags from config.')
+                self.lgd.config.remove_option(game.app_name, 'install_tags')
+            elif config_tags:
+                self.log.info(f'Using install tags from config: {config_tags}')
+                file_install_tag = config_tags.split(',')
 
-        # load old manifest
-        old_manifest = None
-
-        # load old manifest if we have one
-        if override_old_manifest:
-            self.log.info(f'Overriding old manifest with "{override_old_manifest}"')
-            old_bytes, _ = self.get_uri_manifest(override_old_manifest)
-            old_manifest = self.load_manifest(old_bytes)
-        elif not disable_patching and not force and self.is_installed(game.app_name):
-            old_bytes, _base_urls = self.get_installed_manifest(game.app_name)
-            if _base_urls and not game.base_urls:
-                game.base_urls = _base_urls
-
-            if not old_bytes:
-                self.log.error(f'Could not load old manifest, patching will not work!')
-            else:
-                old_manifest = self.load_manifest(old_bytes)
-
-        base_urls = game.base_urls
-
-        # The EGS client uses plaintext HTTP by default for the purposes of enabling simple DNS based
-        # CDN redirection to a (local) cache. In Legendary this will be a config option.
-        disable_https = disable_https or self.lgd.config.getboolean('Legendary', 'disable_https', fallback=False)
-
-        if override_manifest:
-            self.log.info(f'Overriding manifest with "{override_manifest}"')
-            new_manifest_data, _base_urls = self.get_uri_manifest(override_manifest)
-            # if override manifest has a base URL use that instead
-            if _base_urls:
-                base_urls = _base_urls
-        else:
-            new_manifest_data, base_urls = self.get_cdn_manifest(game, platform, disable_https=disable_https)
-            # overwrite base urls in metadata with current ones to avoid using old/dead CDNs
-            game.base_urls = base_urls
-            # save base urls to game metadata
-            self.lgd.set_game_meta(game.app_name, game)
-
-        self.log.info('Parsing game manifest...')
-        new_manifest = self.load_manifest(new_manifest_data)
-        self.log.debug(f'Base urls: {base_urls}')
-        # save manifest with version name as well for testing/downgrading/etc.
-        self.lgd.save_manifest(game.app_name, new_manifest_data,
-                               version=new_manifest.meta.build_version,
-                               platform=platform)
-
-        # check if we should use a delta manifest or not
-        disable_delta = disable_delta or ((override_old_manifest or override_manifest) and not override_delta_manifest)
-        if old_manifest and new_manifest:
-            disable_delta = disable_delta or (old_manifest.meta.build_id == new_manifest.meta.build_id)
-        if old_manifest and new_manifest and not disable_delta:
-            if override_delta_manifest:
-                self.log.info(f'Overriding delta manifest with "{override_delta_manifest}"')
-                delta_manifest_data, _ = self.get_uri_manifest(override_delta_manifest)
-            else:
-                delta_manifest_data = self.get_delta_manifest(base_urls[0],
-                                                              old_manifest.meta.build_id,
-                                                              new_manifest.meta.build_id)
-            if delta_manifest_data:
-                delta_manifest = self.load_manifest(delta_manifest_data)
-                self.log.info(f'Using optimized delta manifest to upgrade from build '
-                              f'"{old_manifest.meta.build_id}" to '
-                              f'"{new_manifest.meta.build_id}"...')
-                combine_manifests(new_manifest, delta_manifest)
-            else:
-                self.log.debug(f'No Delta manifest received from CDN.')
-
-        # reuse existing installation's directory
-        if igame := self.get_installed_game(base_game.app_name if base_game else game.app_name):
-            install_path = igame.install_path
-            # make sure to re-use the epic guid we assigned on first install
-            if not game.is_dlc and igame.egl_guid:
-                egl_guid = igame.egl_guid
-        else:
-            if not game_folder:
-                if game.is_dlc:
-                    game_folder = base_game.metadata.get('customAttributes', {}). \
-                        get('FolderName', {}).get('value', base_game.app_name)
-                else:
-                    game_folder = game.metadata.get('customAttributes', {}). \
-                        get('FolderName', {}).get('value', game.app_name)
-
-            if not base_path:
-                base_path = self.get_default_install_dir(platform=platform)
-
-                if platform == 'Mac':
-                    # if we're on mac and the path to the binary does not start with <something>.app,
-                    # treat it as if it were a Windows game instead and install it to the default folder.
-                    if '.app' not in new_manifest.meta.launch_exe.partition('/')[0].lower():
-                        base_path = self.get_default_install_dir(platform='Windows')
-                    else:
-                        # If it is a .app omit the game folder
-                        game_folder = ''
-
-            # make sure base directory actually exists (but do not create game dir)
-            if not os.path.exists(base_path):
-                self.log.info(f'"{base_path}" does not exist, creating...')
-                os.makedirs(base_path)
-
-            install_path = os.path.normpath(os.path.join(base_path, game_folder))
-
-        # check for write access on the install path or its parent directory if it doesn't exist yet
-        base_path = os.path.dirname(install_path)
-        if os.path.exists(install_path) and not os.access(install_path, os.W_OK):
-            raise PermissionError(f'No write access to "{install_path}"')
-        elif not os.access(base_path, os.W_OK):
-            raise PermissionError(f'No write access to "{base_path}"')
-
-        self.log.info(f'Install path: {install_path}')
-
-        if repair:
-            if not repair_use_latest and old_manifest:
-                # use installed manifest for repairs instead of updating
-                new_manifest = old_manifest
-                old_manifest = None
-
-            filename = clean_filename(f'{game.app_name}.repair')
-            resume_file = os.path.join(self.lgd.get_tmp_path(), filename)
-            force = False
-        elif not force:
-            filename = clean_filename(f'{game.app_name}.resume')
-            resume_file = os.path.join(self.lgd.get_tmp_path(), filename)
-        else:
-            resume_file = None
-
-        # Use user-specified base URL or preferred CDN first, otherwise fall back to
-        # EGS's behaviour of just selecting the first CDN in the list.
-        base_url = None
-        if override_base_url:
-            self.log.info(f'Overriding base URL with "{override_base_url}"')
-            base_url = override_base_url
-        elif preferred_cdn or (preferred_cdn := self.lgd.config.get('Legendary', 'preferred_cdn', fallback=None)):
-            for url in base_urls:
-                if preferred_cdn in url:
-                    base_url = url
-                    break
-            else:
-                self.log.warning(f'Preferred CDN "{preferred_cdn}" unavailable, using default selection.')
-        # Use first, fail if none known
-        if not base_url:
-            if not base_urls:
-                raise ValueError('No base URLs found, please try again.')
-            base_url = base_urls[0]
-
-        if disable_https:
-            base_url = base_url.replace('https://', 'http://')
-
-        self.log.debug(f'Using base URL: {base_url}')
-        scheme, cdn_host = base_url.split('/')[0:3:2]
-        self.log.info(f'Selected CDN: {cdn_host} ({scheme.strip(":")})')
-
-        if not max_shm:
-            max_shm = self.lgd.config.getint('Legendary', 'max_memory', fallback=2048)
-
-        if dl_optimizations or is_opt_enabled(game.app_name, new_manifest.meta.build_version):
-            self.log.info('Download order optimizations are enabled.')
-            process_opt = True
-        else:
-            process_opt = False
-
-        if not max_workers:
-            max_workers = self.lgd.config.getint('Legendary', 'max_workers', fallback=0)
-
-        dlm = DLManager(install_path, base_url, resume_file=resume_file, status_q=status_q,
-                        max_shared_memory=max_shm * 1024 * 1024, max_workers=max_workers,
-                        dl_timeout=dl_timeout)
-        anlres = dlm.run_analysis(manifest=new_manifest, old_manifest=old_manifest,
-                                  patch=not disable_patching, resume=not force,
-                                  file_prefix_filter=file_prefix_filter,
-                                  file_exclude_filter=file_exclude_filter,
-                                  file_install_tag=file_install_tag,
-                                  processing_optimization=process_opt)
-
-        prereq = None
-        if new_manifest.meta.prereq_ids:
-            prereq = dict(ids=new_manifest.meta.prereq_ids, name=new_manifest.meta.prereq_name,
-                          path=new_manifest.meta.prereq_path, args=new_manifest.meta.prereq_args)
-
-        offline = game.metadata.get('customAttributes', {}).get('CanRunOffline', {}).get('value', 'true')
-        ot = game.metadata.get('customAttributes', {}).get('OwnershipToken', {}).get('value', 'false')
-
-        if file_install_tag is None:
-            file_install_tag = []
-        igame = InstalledGame(app_name=game.app_name, title=game.app_title,
-                              version=new_manifest.meta.build_version, prereq_info=prereq,
-                              manifest_path=override_manifest, base_urls=base_urls,
-                              install_path=install_path, executable=new_manifest.meta.launch_exe,
-                              launch_parameters=new_manifest.meta.launch_command,
-                              can_run_offline=offline == 'true', requires_ot=ot == 'true',
-                              is_dlc=base_game is not None, install_size=anlres.install_size,
-                              egl_guid=egl_guid, install_tags=file_install_tag,
-                              platform=platform)
+        dlm, analysis, igame = super(LegendaryCore, self).prepare_download(game=game, base_game=base_game, base_path=base_path,
+                                                          force=force, max_shm=max_shm,
+                                                          max_workers=max_workers, game_folder=game_folder,
+                                                          disable_patching=disable_patching,
+                                                          override_manifest=override_manifest,
+                                                          override_old_manifest=override_old_manifest,
+                                                          override_base_url=override_base_url,
+                                                          platform=platform,
+                                                          file_prefix_filter=file_prefix_filter,
+                                                          file_exclude_filter=file_exclude_filter,
+                                                          file_install_tag=file_install_tag,
+                                                          dl_optimizations=dl_optimizations,
+                                                          dl_timeout=dl_timeout,
+                                                          repair=repair,
+                                                          repair_use_latest=repair_use_latest,
+                                                          disable_delta=disable_delta,
+                                                          override_delta_manifest=override_delta_manifest,
+                                                          preferred_cdn=preferred_cdn,
+                                                          disable_https=disable_https)
 
         # game is either up to date or hasn't changed, so we have nothing to do
-        if not anlres.dl_size:
+        if not analysis.dl_size:
             self.log.info('Download size is 0, the game is either already up to date or has not changed. Exiting...')
             self.clean_post_install(game, igame, repair, repair_file)
 
             raise RuntimeError('Nothing to do.')
 
-        res = self.check_installation_conditions(analysis=anlres, install=igame, game=game,
+        res = self.check_installation_conditions(analysis=analysis, install=igame, game=game,
                                                  updating=self.is_installed(app_name),
                                                  ignore_space_req=ignore_space_req)
 
-        return dlm, anlres, game, igame, repair, repair_file, res
+        return dlm, analysis, igame, game, repair, repair_file, res
 
     def verify_game(self, app_name: str, callback: Callable[[int, int], None] = print):
         if not self.is_installed(app_name):
