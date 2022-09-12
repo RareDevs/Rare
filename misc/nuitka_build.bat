9d2868080492d5c68a1ddf0082f462ee5a7493a0
Stelios Tsampas <loathingkernel@gmail.com>
2022-09-12 14:19:24 +0300

    Build: enable console output on windows if it is a test build

---
+++ misc/nuitka_build.bat
@@ -0,0 +1,28 @@
+python -m nuitka ^
+--assume-yes-for-downloads ^
+--mingw64 ^
+--lto=no ^
+--jobs=2 ^
+--static-libpython=no ^
+--standalone ^
+--enable-plugin=anti-bloat ^
+--enable-plugin=pyqt5 ^
+--show-modules ^
+--show-anti-bloat-changes ^
+--follow-stdlib ^
+--follow-imports ^
+--nofollow-import-to="*.tests" ^
+--nofollow-import-to="*.distutils" ^
+--prefer-source-code ^
+--include-package=pypresence ^
+--include-package-data=qtawesome ^
+--include-data-dir=rare\resources\images=rare\resources\images ^
+--include-data-files=rare\resources\languages=rare\resources\languages="*.qm" ^
+--windows-icon-from-ico=rare\resources\images\Rare.ico ^
+--windows-company-name=Rare ^
+--windows-product-name=Rare ^
+--windows-file-description=rare.exe ^
+--windows-file-version=0.0.0.0 ^
+--windows-product-version=0.0.0.0 ^
+--enable-console ^
+rare
