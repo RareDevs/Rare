16775fb66100aa08b067fd9f9d36f8e0180eba04
lennard <44114474+Dummerle@users.noreply.github.com>
2022-12-08 21:33:31 +0100

    Bump version

--- rare/__init__.py
+++ rare/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.9.3"
+__version__ = "1.9.4"
 
 code_name = "Magnolia Catnip"
--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
