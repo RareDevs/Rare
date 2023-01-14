8004230e197b1ffcbfda82d21068e14ed91c675e
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2023-01-14 20:56:46 +0200

    GameProcess: Use legendary's `Game` instead of `RareGame` as it isn't necessary
    Signed-off-by: loathingKernel <142770+loathingKernel@users.noreply.github.com>

--- rare/shared/game_process.py
+++ rare/shared/game_process.py
@@ -1,18 +1,15 @@
 import json
 import logging
 from enum import IntEnum
-from typing import TYPE_CHECKING
 
 from PyQt5.QtCore import QObject, pyqtSignal, QTimer, pyqtSlot
 from PyQt5.QtNetwork import QLocalSocket
 from PyQt5.QtWidgets import QMessageBox
+from legendary.models.game import Game
 
 from rare.models.launcher import ErrorModel, Actions, FinishedModel, StateChangedModel
 
 logger = logging.getLogger("GameProcess")
-
-if TYPE_CHECKING:
-    from rare.models.game import RareGame
 
 
 class GameProcess(QObject):
@@ -26,35 +23,27 @@ class GameProcess(QObject):
         TIMEOUT = -1
         ON_STARTUP = -1234
 
-    def __init__(self, rgame: 'RareGame'):
+    def __init__(self, game: Game):
         super(GameProcess, self).__init__()
-        self.rgame = rgame
+        self.game = game
         self.on_startup = False
         self.tried_connections = 0
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.__connect)
         self.socket = QLocalSocket()
-        self.socket_name = f"rare_{self.rgame.app_name}"
+        self.socket_name = f"rare_{self.game.app_name}"
 
-        self.socket.connected.connect(self._socket_connected)
-        # if hasattr(self.socket, "errorOccured"):
-        #     self.socket.errorOccurred.connect(self._error_occurred)
-        # else:
-        #     QTimer.singleShot(
-        #         100,
-        #         lambda: self._error_occurred(QLocalSocket.UnknownSocketError) if self.socket.error() else None
-        #     )
-        #     logger.warning("Do not handle errors on QLocalSocket, because of an old qt version")
+        self.socket.connected.connect(self.__on_connected)
         try:
-            self.socket.errorOccurred.connect(self._error_occurred)
+            self.socket.errorOccurred.connect(self.__on_error)
         except AttributeError:
             # QTimer.singleShot(
             #     100,
             #     lambda: self._error_occurred(QLocalSocket.UnknownSocketError) if self.socket.error() else None
             # )
             logger.warning("Do not handle errors on QLocalSocket, because of an old qt version")
-        self.socket.readyRead.connect(self._message_available)
+        self.socket.readyRead.connect(self.__on_message)
         self.socket.disconnected.connect(self.__close)
 
     def connect(self, on_startup: bool):
@@ -79,7 +68,7 @@ class GameProcess(QObject):
             self.finished.emit(GameProcess.Code.TIMEOUT)
 
     @pyqtSlot()
-    def _message_available(self):
+    def __on_message(self):
         message = self.socket.readAll().data()
         if not message.startswith(b"{"):
             logger.error(f"Received unsupported message: {message.decode('utf-8')}")
@@ -96,17 +85,17 @@ class GameProcess(QObject):
         if not action:
             logger.error("Got unexpected action")
         elif action == Actions.finished:
-            logger.info(f"{self.rgame.app_name} {self.rgame.app_title} finished")
+            logger.info(f"{self.game.app_name} {self.game.app_title} finished")
             model = FinishedModel.from_json(data)
             self.socket.close()
-            self._game_finished(model.exit_code)
+            self.__game_finished(model.exit_code)
         elif action == Actions.error:
             model = ErrorModel.from_json(data)
-            logger.error(f"Error in game {self.rgame.app_title}: {model.error_string}")
+            logger.error(f"Error in game {self.game.app_title}: {model.error_string}")
             self.socket.close()
-            self._game_finished(1)
+            self.__game_finished(1)
             QMessageBox.warning(None, "Error", self.tr(
-                "Error in game {}:\n{}").format(self.rgame.app_title, model.error_string))
+                "Error in game {}:\n{}").format(self.game.app_title, model.error_string))
 
         elif action == Actions.state_update:
             model = StateChangedModel.from_json(data)
@@ -115,20 +104,20 @@ class GameProcess(QObject):
                 self.launched.emit(GameProcess.Code.SUCCESS)
 
     @pyqtSlot()
-    def _socket_connected(self):
+    def __on_connected(self):
         self.timer.stop()
-        logger.info(f"Connection established for {self.rgame.app_name} ({self.rgame.app_title})")
+        logger.info(f"Connection established for {self.game.app_name} ({self.game.app_title})")
         if self.on_startup:
-            logger.info(f"Found {self.rgame.app_name} ({self.rgame.app_title}) running at startup")
+            logger.info(f"Found {self.game.app_name} ({self.game.app_title}) running at startup")
             self.launched.emit(GameProcess.Code.ON_STARTUP)
 
     @pyqtSlot(QLocalSocket.LocalSocketError)
-    def _error_occurred(self, _: QLocalSocket.LocalSocketError):
+    def __on_error(self, _: QLocalSocket.LocalSocketError):
         if self.on_startup:
             self.timer.stop()
-            self.socket.close()
-            self._game_finished(GameProcess.Code.ON_STARTUP)  # 1234 is exit code for startup
-        logger.error(f"{self.rgame.app_name} ({self.rgame.app_title}): {self.socket.errorString()}")
+            self.__close()
+            self.__game_finished(GameProcess.Code.ON_STARTUP)  # 1234 is exit code for startup
+        logger.error(f"{self.game.app_name} ({self.game.app_title}): {self.socket.errorString()}")
 
-    def _game_finished(self, exit_code: int):
+    def __game_finished(self, exit_code: int):
         self.finished.emit(exit_code)
