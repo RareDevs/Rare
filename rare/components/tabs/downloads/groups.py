c523fa92105ee2b97dbf6882ffaa2b8d652eced5
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2023-01-25 13:51:36 +0200

    UpdateGroup: Keep updates' app_names in a list too.
    Because `deleteLater()` doesn't delete the widget immediately (duh!)
    `count()` and `contains()` can report false results while the widget
    is queued for deletion.
    
    Hide container and list/queue by name mangling.
    
    Remove test code.

--- rare/components/tabs/downloads/groups.py
+++ rare/components/tabs/downloads/groups.py
@@ -2,13 +2,15 @@ from argparse import Namespace
 from collections import deque
 from enum import IntEnum
 from logging import getLogger
-from typing import Optional
+from typing import Optional, List, Deque
 
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import (
+    QWidget,
     QGroupBox,
     QVBoxLayout,
-    QLabel, QSizePolicy, QWidget,
+    QLabel,
+    QSizePolicy,
 )
 from legendary.models.game import Game, InstalledGame
 
@@ -25,48 +27,55 @@ class UpdateGroup(QGroupBox):
         super(UpdateGroup, self).__init__(parent=parent)
         self.setObjectName(type(self).__name__)
         self.setTitle(self.tr("Updates"))
-        self.text = QLabel(self.tr("No updates available"))
-        self.text.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.__text = QLabel(self.tr("No updates available"))
+        self.__text.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
         # lk: For findChildren to work, the upates's layout has to be in a widget
-        self.container = QWidget(self)
-        self.container.setLayout(QVBoxLayout())
-        self.container.layout().setContentsMargins(0, 0, 0, 0)
+        self.__container = QWidget(self)
+        self.__container.setLayout(QVBoxLayout())
+        self.__container.layout().setContentsMargins(0, 0, 0, 0)
 
         self.setLayout(QVBoxLayout())
-        self.layout().addWidget(self.text)
-        self.layout().addWidget(self.container)
+        self.layout().addWidget(self.__text)
+        self.layout().addWidget(self.__container)
+
+        self.__list: List[str] = []
 
     @staticmethod
     def __widget_name(app_name: str) -> str:
         return f"UpdateWidget_{app_name}"
 
     def __find_widget(self, app_name: str) -> Optional[UpdateWidget]:
-        return self.container.findChild(UpdateWidget, name=self.__widget_name(app_name))
+        return self.__container.findChild(UpdateWidget, name=self.__widget_name(app_name))
 
     def count(self) -> int:
-        return len(self.container.findChildren(UpdateWidget, options=Qt.FindDirectChildrenOnly))
+        return len(self.__list)
 
     def contains(self, app_name: str) -> bool:
-        return  self.__find_widget(app_name) is not None
+        if app_name in self.__list:
+            return self.__find_widget(app_name) is not None
+        else:
+            return False
 
     def append(self, game: Game, igame: InstalledGame):
-        self.text.setVisible(False)
-        self.container.setVisible(True)
+        self.__text.setVisible(False)
+        self.__container.setVisible(True)
+        self.__list.append(game.app_name)
         widget: UpdateWidget = self.__find_widget(game.app_name)
         if widget is not None:
-            self.container.layout().removeWidget(widget)
+            self.__container.layout().removeWidget(widget)
             widget.deleteLater()
-        widget = UpdateWidget(game, igame, parent=self.container)
+        widget = UpdateWidget(game, igame, parent=self.__container)
         widget.enqueue.connect(self.enqueue)
-        self.container.layout().addWidget(widget)
+        self.__container.layout().addWidget(widget)
 
     def remove(self, app_name: str):
+        self.__list.remove(app_name)
         widget: UpdateWidget = self.__find_widget(app_name)
-        self.container.layout().removeWidget(widget)
+        self.__container.layout().removeWidget(widget)
         widget.deleteLater()
-        self.text.setVisible(not bool(self.count()))
-        self.container.setVisible(bool(self.count()))
+        self.__text.setVisible(not bool(self.count()))
+        self.__container.setVisible(bool(self.count()))
 
     def set_widget_enabled(self, app_name: str, enabled: bool):
         widget: UpdateWidget = self.__find_widget(app_name)
@@ -85,37 +94,40 @@ class QueueGroup(QGroupBox):
         super(QueueGroup, self).__init__(parent=parent)
         self.setObjectName(type(self).__name__)
         self.setTitle(self.tr("Queue"))
-        self.text = QLabel(self.tr("No downloads in queue"), self)
-        self.text.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.__text = QLabel(self.tr("No downloads in queue"), self)
+        self.__text.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
         # lk: For findChildren to work, the queue's layout has to be in a widget
-        self.container = QWidget(self)
-        self.container.setLayout(QVBoxLayout())
-        self.container.layout().setContentsMargins(0, 0, 0, 0)
-        self.container.setVisible(False)
+        self.__container = QWidget(self)
+        self.__container.setLayout(QVBoxLayout())
+        self.__container.layout().setContentsMargins(0, 0, 0, 0)
+        self.__container.setVisible(False)
 
         self.setLayout(QVBoxLayout())
-        self.layout().addWidget(self.text)
-        self.layout().addWidget(self.container)
+        self.layout().addWidget(self.__text)
+        self.layout().addWidget(self.__container)
 
-        self.queue = deque()
+        self.__queue: Deque[str] = deque()
 
     @staticmethod
     def __widget_name(app_name:str) -> str:
         return f"QueueWidget_{app_name}"
 
     def __find_widget(self, app_name: str) -> Optional[QueueWidget]:
-        return self.container.findChild(QueueWidget, name=self.__widget_name(app_name))
+        return self.__container.findChild(QueueWidget, name=self.__widget_name(app_name))
 
     def contains(self, app_name: str) -> bool:
-        return  self.__find_widget(app_name) is not None
+        if app_name in self.__queue:
+            return  self.__find_widget(app_name) is not None
+        else:
+            return False
 
     def count(self) -> int:
-        return len(self.queue)
+        return len(self.__queue)
 
     def __create_widget(self, item: InstallQueueItemModel, old_igame: InstalledGame) -> QueueWidget:
-        widget: QueueWidget = QueueWidget(item, old_igame, parent=self.container)
-        widget.toggle_arrows(self.queue.index(item.download.game.app_name), len(self.queue))
+        widget: QueueWidget = QueueWidget(item, old_igame, parent=self.__container)
+        widget.toggle_arrows(self.__queue.index(item.download.game.app_name), len(self.__queue))
         widget.remove.connect(self.remove)
         widget.force.connect(self.__on_force)
         widget.move_up.connect(self.__on_move_up)
@@ -123,34 +135,34 @@ class QueueGroup(QGroupBox):
         return widget
 
     def push_front(self, item: InstallQueueItemModel, old_igame: InstalledGame):
-        self.text.setVisible(False)
-        self.container.setVisible(True)
-        self.queue.appendleft(item.download.game.app_name)
+        self.__text.setVisible(False)
+        self.__container.setVisible(True)
+        self.__queue.appendleft(item.download.game.app_name)
         widget = self.__create_widget(item, old_igame)
-        self.container.layout().insertWidget(0, widget)
+        self.__container.layout().insertWidget(0, widget)
         if self.count() > 1:
-            app_name = self.queue[1]
+            app_name = self.__queue[1]
             other: QueueWidget =  self.__find_widget(app_name)
-            other.toggle_arrows(1, len(self.queue))
+            other.toggle_arrows(1, len(self.__queue))
 
     def push_back(self, item: InstallQueueItemModel, old_igame: InstalledGame):
-        self.text.setVisible(False)
-        self.container.setVisible(True)
-        self.queue.append(item.download.game.app_name)
+        self.__text.setVisible(False)
+        self.__container.setVisible(True)
+        self.__queue.append(item.download.game.app_name)
         widget = self.__create_widget(item, old_igame)
-        self.container.layout().addWidget(widget)
+        self.__container.layout().addWidget(widget)
         if self.count() > 1:
-            app_name = self.queue[-2]
+            app_name = self.__queue[-2]
             other: QueueWidget =  self.__find_widget(app_name)
-            other.toggle_arrows(len(self.queue) - 2, len(self.queue))
+            other.toggle_arrows(len(self.__queue) - 2, len(self.__queue))
 
     def pop_front(self) -> InstallQueueItemModel:
-        app_name = self.queue.popleft()
+        app_name = self.__queue.popleft()
         widget: QueueWidget = self.__find_widget(app_name)
         item = widget.item
         widget.deleteLater()
-        self.text.setVisible(not bool(self.count()))
-        self.container.setVisible(bool(self.count()))
+        self.__text.setVisible(not bool(self.count()))
+        self.__container.setVisible(bool(self.count()))
         return item
 
     def __update_queue(self):
@@ -161,23 +173,23 @@ class QueueGroup(QGroupBox):
         """
         for idx in [0, 1]:
             if self.count() > idx:
-                app_name = self.queue[idx]
+                app_name = self.__queue[idx]
                 widget: QueueWidget =  self.__find_widget(app_name)
-                widget.toggle_arrows(idx, len(self.queue))
+                widget.toggle_arrows(idx, len(self.__queue))
         for idx in [1, 2]:
             if self.count() > idx:
-                app_name = self.queue[-idx]
+                app_name = self.__queue[-idx]
                 widget: QueueWidget =  self.__find_widget(app_name)
-                widget.toggle_arrows(len(self.queue) - idx, len(self.queue))
+                widget.toggle_arrows(len(self.__queue) - idx, len(self.__queue))
 
     def __remove(self, app_name: str):
-        self.queue.remove(app_name)
+        self.__queue.remove(app_name)
         widget: QueueWidget = self.__find_widget(app_name)
-        self.container.layout().removeWidget(widget)
+        self.__container.layout().removeWidget(widget)
         widget.deleteLater()
         self.__update_queue()
-        self.text.setVisible(not bool(self.count()))
-        self.container.setVisible(bool(self.count()))
+        self.__text.setVisible(not bool(self.count()))
+        self.__container.setVisible(bool(self.count()))
 
     @pyqtSlot(str)
     def remove(self, app_name: str):
@@ -200,11 +212,11 @@ class QueueGroup(QGroupBox):
         :param direction: -1 to move up, +1 to move down
         :return: None
         """
-        index = self.queue.index(app_name)
-        self.queue.remove(app_name)
-        self.queue.insert(index + int(direction), app_name)
+        index = self.__queue.index(app_name)
+        self.__queue.remove(app_name)
+        self.__queue.insert(index + int(direction), app_name)
         widget: QueueWidget = self.__find_widget(app_name)
-        self.container.layout().insertWidget(index + int(direction), widget)
+        self.__container.layout().insertWidget(index + int(direction), widget)
         self.__update_queue()
 
     @pyqtSlot(str)
@@ -214,29 +226,3 @@ class QueueGroup(QGroupBox):
     @pyqtSlot(str)
     def __on_move_down(self, app_name: str):
         self.__move(app_name, QueueGroup.MoveDirection.DOWN)
-
-
-if __name__ == "__main__":
-    import sys
-    from PyQt5.QtWidgets import QApplication, QDialog
-    from rare.utils.misc import set_style_sheet
-
-    app = QApplication(sys.argv)
-
-    set_style_sheet("RareStyle")
-
-    queue_group = QueueGroup()
-
-    dialog = QDialog()
-    dialog.setLayout(QVBoxLayout())
-    dialog.layout().addWidget(queue_group)
-    for i in range(5):
-        rgame = Namespace(app_name=i, title=f"{i}", remote_version=f"{i}", version=f"{i}")
-        analysis = Namespace(dl_size=i*1024, install_size=i*2048)
-        game = Namespace(app_name=f"{i}")
-        download = Namespace(analysis=analysis, game=game)
-        model = InstallQueueItemModel(rgame=rgame, download=download, options=True)
-        queue_group.push_back(model)
-    dialog.layout().setSizeConstraint(QVBoxLayout.SetFixedSize)
-    dialog.show()
-    sys.exit(app.exec_())
