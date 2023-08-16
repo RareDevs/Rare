95fba89277b54aab66bb631cf223bfbf9a049232
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2023-08-16 20:55:41 +0300

    Wrapper: Truncate text on widgets to show only the command
    Truncate the widget's text down to the command only without the arguments.
    Show the full command with the arguments in the tooltip instead.
    
    Update some text.

--- rare/components/tabs/settings/widgets/wrapper.py
+++ rare/components/tabs/settings/widgets/wrapper.py
@@ -4,7 +4,7 @@ from logging import getLogger
 from typing import Dict, List
 
 from PyQt5.QtCore import pyqtSignal, QSettings, QSize, Qt, QMimeData, pyqtSlot, QCoreApplication
-from PyQt5.QtGui import QDrag, QDropEvent, QDragEnterEvent, QDragMoveEvent, QFont
+from PyQt5.QtGui import QDrag, QDropEvent, QDragEnterEvent, QDragMoveEvent, QFont, QMouseEvent
 from PyQt5.QtWidgets import (
     QHBoxLayout,
     QLabel,
@@ -40,7 +40,7 @@ class WrapperWidget(QFrame):
     def __init__(self, text: str, show_text=None, parent=None):
         super(WrapperWidget, self).__init__(parent=parent)
         if not show_text:
-            show_text = text
+            show_text = text.split(" ")[0]
 
         self.setFrameShape(QFrame.StyledPanel)
         self.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Fixed)
@@ -50,14 +50,17 @@ class WrapperWidget(QFrame):
         self.text_lbl.setFont(QFont("monospace"))
         self.image_lbl = QLabel(parent=self)
         self.image_lbl.setPixmap(icon("mdi.drag-vertical").pixmap(QSize(20, 20)))
+        self.setToolTip(text)
 
         self.delete_button = QPushButton(icon("ei.remove"), "", parent=self)
         if show_text in extra_wrapper_regex.keys():
             self.delete_button.setDisabled(True)
-            self.delete_button.setToolTip(self.tr("Disable it in settings"))
+            self.delete_button.setToolTip(self.tr("Disable in settings"))
+        else:
+            self.delete_button.setToolTip(self.tr("Remove"))
         self.delete_button.clicked.connect(self.delete)
 
-        layout = QHBoxLayout()
+        layout = QHBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.image_lbl)
         layout.addWidget(self.text_lbl)
@@ -77,6 +80,11 @@ class WrapperWidget(QFrame):
             mime = QMimeData()
             drag.setMimeData(mime)
             drag.exec_(Qt.MoveAction)
+
+    def mouseDoubleClickEvent(self, a0: QMouseEvent) -> None:
+        if a0.button() == Qt.LeftButton:
+            a0.accept()
+            pass  # self.edit
 
 
 class WrapperSettings(QWidget, Ui_WrapperSettings):
@@ -155,26 +163,34 @@ class WrapperSettings(QWidget, Ui_WrapperSettings):
     def add_wrapper(self, text: str, from_load=False):
         if text == "mangohud" and self.wrappers.get("mangohud"):
             return
-        show_text = text
+        show_text = ""
         for key, extra_wrapper in extra_wrapper_regex.items():
             if re.match(extra_wrapper, text):
                 show_text = key
+        if not show_text:
+            show_text = text.split()[0]
 
         # validate
         if not text.strip():  # is empty
             return
         if not from_load:
             if self.wrappers.get(text):
-                QMessageBox.warning(self, "Warning", self.tr("Wrapper is already in the list"))
+                QMessageBox.warning(
+                    self, self.tr("Warning"), self.tr("Wrapper is already in the list")
+                )
                 return
 
             if show_text != "proton" and not shutil.which(text.split()[0]):
-                if QMessageBox.question(self, "Warning", self.tr("Wrapper is not in $PATH. Ignore? "),
-                                        QMessageBox.Yes | QMessageBox.No, QMessageBox.No) == QMessageBox.No:
+                if QMessageBox.question(
+                    self, self.tr("Warning"), self.tr("Wrapper is not in $PATH. Ignore? "),
+                    QMessageBox.Yes | QMessageBox.No, QMessageBox.No
+                ) == QMessageBox.No:
                     return
 
             if text == "proton":
-                QMessageBox.warning(self, "Warning", self.tr("Do not insert proton manually. Add it in proton settings"))
+                QMessageBox.warning(
+                    self, self.tr("Warning"), self.tr("Do not insert proton manually. Add it through Proton settings")
+                )
                 return
 
         self.widget_stack.setCurrentIndex(0)
