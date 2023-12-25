ce97ec90868bb0de615f82217a4226182cb333a8
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2023-12-25 12:34:22 +0200

    Paths: Check for membership

--- rare/utils/paths.py
+++ rare/utils/paths.py
@@ -141,7 +141,7 @@ def get_rare_executable() -> List[str]:
         executable = [sys.executable]
     elif sys.argv[0].endswith("__main__.py"):
         executable = [sys.executable, "-m", "rare"]
-    elif platform.system() == "Linux" or platform.system() == "Darwin" or platform.system() == "FreeBSD":
+    elif platform.system() in {"Linux", "FreeBSD", "Darwin"}:
         if p := os.environ.get("APPIMAGE"):
             executable = [p]
         else:
@@ -214,7 +214,7 @@ def create_desktop_link(app_name: str, app_title: str = "", link_name: str = "",
     else:
         logger.info(f"Creating shortcut for {app_title} at {shortcut_path}")
 
-    if platform.system() == "Linux" or platform.system() == "FreeBSD":
+    if platform.system() in {"Linux", "FreeBSD"}:
         executable = get_rare_executable()
         executable = shlex.join(executable)
         if not for_rare:
