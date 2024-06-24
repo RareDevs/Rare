a921e2b13a62dcfbdb57e786e0f1f79aa8a7a888
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2024-06-24 16:16:57 +0300

    repo: move pyside6 script to their regular names

--- misc/py2ts-pyside6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cwd="$(pwd)"
-cd "$(dirname "$0")"/.. || exit
-
-pylupdate5 -noobsolete $(find rare/ -iname "*.py") -ts rare/resources/languages/translation_source.ts
-
-cd "$cwd" || exit
