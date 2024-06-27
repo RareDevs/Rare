cc9c6232d647527e4160fe0442dd42b13b3733ad
Stelios Tsampas <loathingkernel@gmail.com>
2024-06-27 21:25:36 +0300

    RareLauncher: in pyside6 the socket is already deleted

--- rare/commands/launcher/__init__.py
+++ rare/commands/launcher/__init__.py
@@ -228,7 +228,6 @@ class RareLauncher(RareApp):
 
     def socket_disconnected(self):
         self.logger.info("Server disconnected")
-        self.socket.deleteLater()
         self.socket = None
 
     def send_message(self, message: BaseModel):
