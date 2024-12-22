d776f55ea3b2e2c5f48d38ce84de5dd108759f7e
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2024-12-22 23:29:14 +0200

    workflows: bump python to 3.13

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-import setuptools
+from setuptools import setup, find_packages
+from setuptools_scm import ScmVersion
 
 from rare import __version__ as version
 
@@ -19,22 +20,24 @@ optional_reqs = dict(
     pypresence=parse_requirements("requirements-presence.txt"),
 )
 
-setuptools.setup(
+setup(
     name="Rare",
     version=version,
+    use_scm_version={"version_scheme": "only-version", "local_scheme": "no-local-version"},
     author="RareDevs",
     license="GPL-3",
     description="A gui for legendary",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Dummerle/Rare",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         "Operating System :: OS Independent"
     ],
