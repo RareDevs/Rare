2498c8e4a63a2c448805ec492c57142da072277b
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2024-12-30 15:54:23 +0200

    rare: add attributes to `__all__`

--- rare/__init__.py
+++ rare/__init__.py
@@ -6,4 +6,7 @@ __codename__ = "Garlic Crab"
 if __name__ == "__main__":
     import sys
     from rare.main import main
+
     sys.exit(main())
+
+__all__ = ["__version__", "__version_tuple__", "__codename__"]
