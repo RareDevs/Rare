ab783f2ffd11be068277677069bb3c96580de04f
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-02-25 21:42:37 +0200

    workflows: rebuild resources, translations and ui files while building

--- tools/ts2qm.py
+++ tools/ts2qm.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import os
+import shlex
 
 for f in os.listdir(os.path.join(os.path.dirname(__file__), "../rare/resources/languages/")):
-    if f.endswith(".ts") and f != "translation_source.ts":
-        os.system(f"pyside6-lrelease {os.path.join(os.path.dirname(__file__), '../rare/resources/languages/', f)}")
+    if f.endswith(".ts") and f != "source.ts":
+        os.system(shlex.join((
+            "pyside6-lrelease",
+            "-compress",
+            "-removeidentical",
+            f"{os.path.join(os.path.dirname(__file__), '../rare/resources/languages/', f)}",
+        )))
