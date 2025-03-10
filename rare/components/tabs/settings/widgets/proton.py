dd73c1936f468231d89ceffc3349ed1809940257
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-03-10 22:06:57 +0200

    ConfigHelper: refactor save functions
    rename `save_*` functions to `adjust_` to better reflect their nature.
    These function set or remove the an option depending on the value, they
    do not write to the disk.

--- rare/components/tabs/settings/widgets/proton.py
+++ rare/components/tabs/settings/widgets/proton.py
@@ -94,7 +94,7 @@ class ProtonSettings(QGroupBox):
 
         steam_environ = steam.get_steam_environment(steam_tool, self.tool_prefix.text())
         for key, value in steam_environ.items():
-            config.save_envvar(self.app_name, key, value)
+            config.adjust_envvar(self.app_name, key, value)
             self.environ_changed.emit(key)
 
         wrappers = self.wrappers.get_game_wrapper_list(self.app_name)
@@ -114,7 +114,7 @@ class ProtonSettings(QGroupBox):
         if steam_tool:
             if not (compatdata_path := config.get_proton_compatdata(self.app_name, fallback="")):
                 compatdata_path = proton_compat_dir(RareCore.instance().get_game(self.app_name).folder_name)
-                config.save_proton_compatdata(self.app_name, str(compatdata_path))
+                config.adjust_proton_compatdata(self.app_name, str(compatdata_path))
             self.tool_prefix.setText(str(compatdata_path))
         else:
             self.tool_prefix.setText("")
@@ -131,6 +131,6 @@ class ProtonSettings(QGroupBox):
     def proton_prefix_save(self, text: str):
         if not text:
             return
-        config.save_proton_compatdata(self.app_name, text)
+        config.adjust_proton_compatdata(self.app_name, text)
         self.environ_changed.emit("STEAM_COMPAT_DATA_PATH")
 
