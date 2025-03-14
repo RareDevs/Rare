311e6857771eca8f03755e0343719bb089879c09
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-03-14 23:42:21 +0200

    ui: fix ui python files with ruff after generating them

--- rare/ui/components/dialogs/login/import_login.py
+++ rare/ui/components/dialogs/login/import_login.py
@@ -3,21 +3,16 @@
 ################################################################################
 ## Form generated from reading UI file 'import_login.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.8.1
+## Created by: Qt User Interface Compiler version 6.8.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QFormLayout, QHBoxLayout,
+from PySide6.QtCore import (QCoreApplication, Qt)
+from PySide6.QtGui import (QFont)
+from PySide6.QtWidgets import (QComboBox, QFormLayout, QHBoxLayout,
     QLabel, QLayout, QPushButton, QSizePolicy,
-    QSpacerItem, QVBoxLayout, QWidget)
+    QSpacerItem, QVBoxLayout)
 
 class Ui_ImportLogin(object):
     def setupUi(self, ImportLogin):
--- rare/ui/components/tabs/downloads/download_widget.py
+++ rare/ui/components/tabs/downloads/download_widget.py
@@ -3,20 +3,15 @@
 ################################################################################
 ## Form generated from reading UI file 'download_widget.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.8.1
+## Created by: Qt User Interface Compiler version 6.8.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QProgressBar,
-    QPushButton, QSizePolicy, QWidget)
+from PySide6.QtCore import (QCoreApplication, Qt)
+from PySide6.QtGui import (QFont)
+from PySide6.QtWidgets import (QGridLayout, QLabel, QProgressBar,
+    QPushButton, QSizePolicy)
 
 class Ui_DownloadWidget(object):
     def setupUi(self, DownloadWidget):
