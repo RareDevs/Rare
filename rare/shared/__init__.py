e5bd6d6d8f542cfffb75d513c7baddd73a342aff
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-08-24 11:26:08 +0300

    RareSettings: destroy and delete RareSettings object on exit

--- rare/shared/__init__.py
+++ rare/shared/__init__.py
@@ -5,9 +5,6 @@ Each of the objects in this module should be instantiated ONCE
 and only ONCE!
 """
 
-import logging
-
-from .image_manager import ImageManager
 from .rare_core import RareCore
 
-logger = logging.getLogger("Shared")
+__all__ = ['RareCore']
