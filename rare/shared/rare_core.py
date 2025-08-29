27d373c4eb6d7c00e78774848a7537a7de5ba427
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-08-29 19:03:36 +0300

    RareCore: update graphql url

--- rare/shared/rare_core.py
+++ rare/shared/rare_core.py
@@ -162,7 +162,7 @@ class RareCore(QObject):
                     config_file.write("[Legendary]")
                 self.__core = LegendaryCore()
 
-            self.__core.egs._store_gql_host = "store.epicgames.com"
+            self.__core.egs._store_gql_host = "launcher.store.epicgames.com"
 
             # Initialize sections if they don't exist
             for section in ["Legendary", "default", "default.env"]:
