f41b68afcc849b7ed0ccb0fec858e8b6f5a0f242
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-09-18 14:09:59 +0300

    rare/ui: move install dialog ui files

--- rare/ui/components/dialogs/install/advanced.py
+++ rare/ui/components/dialogs/install/advanced.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'install_dialog_advanced.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.8.2
+## Created by: Qt User Interface Compiler version 6.9.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -11,8 +11,7 @@
 from PySide6.QtCore import (QCoreApplication, Qt)
 from PySide6.QtGui import (QFont)
 from PySide6.QtWidgets import (QCheckBox, QFormLayout, QFrame,
-    QHBoxLayout, QLabel, QPushButton, QSizePolicy,
-    QSpinBox)
+    QHBoxLayout, QLabel, QListWidget, QPushButton, QSizePolicy, QSpinBox)
 
 class Ui_InstallDialogAdvanced(object):
     def setupUi(self, InstallDialogAdvanced):
@@ -27,7 +26,7 @@ class Ui_InstallDialogAdvanced(object):
         self.max_workers_label.setObjectName(u"max_workers_label")
         self.max_workers_label.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
 
-        self.install_dialog_advanced_layout.setWidget(0, QFormLayout.LabelRole, self.max_workers_label)
+        self.install_dialog_advanced_layout.setWidget(0, QFormLayout.ItemRole.LabelRole, self.max_workers_label)
 
         self.max_workers_layout = QHBoxLayout()
         self.max_workers_layout.setObjectName(u"max_workers_layout")
@@ -50,13 +49,13 @@ class Ui_InstallDialogAdvanced(object):
         self.max_workers_layout.addWidget(self.max_workers_info)
 
 
-        self.install_dialog_advanced_layout.setLayout(0, QFormLayout.FieldRole, self.max_workers_layout)
+        self.install_dialog_advanced_layout.setLayout(0, QFormLayout.ItemRole.FieldRole, self.max_workers_layout)
 
         self.max_memory_label = QLabel(InstallDialogAdvanced)
         self.max_memory_label.setObjectName(u"max_memory_label")
         self.max_memory_label.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
 
-        self.install_dialog_advanced_layout.setWidget(1, QFormLayout.LabelRole, self.max_memory_label)
+        self.install_dialog_advanced_layout.setWidget(1, QFormLayout.ItemRole.LabelRole, self.max_memory_label)
 
         self.max_memory_layout = QHBoxLayout()
         self.max_memory_layout.setObjectName(u"max_memory_layout")
@@ -78,12 +77,12 @@ class Ui_InstallDialogAdvanced(object):
         self.max_memory_layout.addWidget(self.max_memory_info)
 
 
-        self.install_dialog_advanced_layout.setLayout(1, QFormLayout.FieldRole, self.max_memory_layout)
+        self.install_dialog_advanced_layout.setLayout(1, QFormLayout.ItemRole.FieldRole, self.max_memory_layout)
 
         self.install_prereqs_label = QLabel(InstallDialogAdvanced)
         self.install_prereqs_label.setObjectName(u"install_prereqs_label")
 
-        self.install_dialog_advanced_layout.setWidget(2, QFormLayout.LabelRole, self.install_prereqs_label)
+        self.install_dialog_advanced_layout.setWidget(2, QFormLayout.ItemRole.LabelRole, self.install_prereqs_label)
 
         self.install_prereqs_check = QCheckBox(InstallDialogAdvanced)
         self.install_prereqs_check.setObjectName(u"install_prereqs_check")
@@ -91,57 +90,57 @@ class Ui_InstallDialogAdvanced(object):
         self.install_prereqs_check.setText(u"")
         self.install_prereqs_check.setChecked(False)
 
-        self.install_dialog_advanced_layout.setWidget(2, QFormLayout.FieldRole, self.install_prereqs_check)
+        self.install_dialog_advanced_layout.setWidget(2, QFormLayout.ItemRole.FieldRole, self.install_prereqs_check)
 
         self.dl_optimizations_label = QLabel(InstallDialogAdvanced)
         self.dl_optimizations_label.setObjectName(u"dl_optimizations_label")
 
-        self.install_dialog_advanced_layout.setWidget(3, QFormLayout.LabelRole, self.dl_optimizations_label)
+        self.install_dialog_advanced_layout.setWidget(3, QFormLayout.ItemRole.LabelRole, self.dl_optimizations_label)
 
         self.dl_optimizations_check = QCheckBox(InstallDialogAdvanced)
         self.dl_optimizations_check.setObjectName(u"dl_optimizations_check")
         self.dl_optimizations_check.setText(u"")
         self.dl_optimizations_check.setChecked(False)
 
-        self.install_dialog_advanced_layout.setWidget(3, QFormLayout.FieldRole, self.dl_optimizations_check)
+        self.install_dialog_advanced_layout.setWidget(3, QFormLayout.ItemRole.FieldRole, self.dl_optimizations_check)
 
         self.force_download_label = QLabel(InstallDialogAdvanced)
         self.force_download_label.setObjectName(u"force_download_label")
 
-        self.install_dialog_advanced_layout.setWidget(4, QFormLayout.LabelRole, self.force_download_label)
+        self.install_dialog_advanced_layout.setWidget(4, QFormLayout.ItemRole.LabelRole, self.force_download_label)
 
         self.force_download_check = QCheckBox(InstallDialogAdvanced)
         self.force_download_check.setObjectName(u"force_download_check")
         self.force_download_check.setText(u"")
 
-        self.install_dialog_advanced_layout.setWidget(4, QFormLayout.FieldRole, self.force_download_check)
+        self.install_dialog_advanced_layout.setWidget(4, QFormLayout.ItemRole.FieldRole, self.force_download_check)
 
         self.ignore_space_label = QLabel(InstallDialogAdvanced)
         self.ignore_space_label.setObjectName(u"ignore_space_label")
 
-        self.install_dialog_advanced_layout.setWidget(5, QFormLayout.LabelRole, self.ignore_space_label)
+        self.install_dialog_advanced_layout.setWidget(5, QFormLayout.ItemRole.LabelRole, self.ignore_space_label)
 
         self.ignore_space_check = QCheckBox(InstallDialogAdvanced)
         self.ignore_space_check.setObjectName(u"ignore_space_check")
         self.ignore_space_check.setFont(font)
 
-        self.install_dialog_advanced_layout.setWidget(5, QFormLayout.FieldRole, self.ignore_space_check)
+        self.install_dialog_advanced_layout.setWidget(5, QFormLayout.ItemRole.FieldRole, self.ignore_space_check)
 
         self.download_only_label = QLabel(InstallDialogAdvanced)
         self.download_only_label.setObjectName(u"download_only_label")
 
-        self.install_dialog_advanced_layout.setWidget(6, QFormLayout.LabelRole, self.download_only_label)
+        self.install_dialog_advanced_layout.setWidget(6, QFormLayout.ItemRole.LabelRole, self.download_only_label)
 
         self.download_only_check = QCheckBox(InstallDialogAdvanced)
         self.download_only_check.setObjectName(u"download_only_check")
         self.download_only_check.setFont(font)
 
-        self.install_dialog_advanced_layout.setWidget(6, QFormLayout.FieldRole, self.download_only_check)
+        self.install_dialog_advanced_layout.setWidget(6, QFormLayout.ItemRole.FieldRole, self.download_only_check)
 
         self.exclude_prefix_label = QLabel(InstallDialogAdvanced)
         self.exclude_prefix_label.setObjectName(u"exclude_prefix_label")
 
-        self.install_dialog_advanced_layout.setWidget(7, QFormLayout.LabelRole, self.exclude_prefix_label)
+        self.install_dialog_advanced_layout.setWidget(7, QFormLayout.ItemRole.LabelRole, self.exclude_prefix_label)
 
         self.exclude_prefix_layout = QHBoxLayout()
         self.exclude_prefix_layout.setObjectName(u"exclude_prefix_layout")
@@ -160,7 +159,12 @@ class Ui_InstallDialogAdvanced(object):
 
         self.exclude_prefix_layout.setStretch(0, 1)
 
-        self.install_dialog_advanced_layout.setLayout(7, QFormLayout.FieldRole, self.exclude_prefix_layout)
+        self.install_dialog_advanced_layout.setLayout(7, QFormLayout.ItemRole.FieldRole, self.exclude_prefix_layout)
+
+        self.exclude_list = QListWidget(InstallDialogAdvanced)
+        self.exclude_list.setObjectName(u"exclude_list")
+
+        self.install_dialog_advanced_layout.setWidget(8, QFormLayout.ItemRole.FieldRole, self.exclude_list)
 
 
         self.retranslateUi(InstallDialogAdvanced)
