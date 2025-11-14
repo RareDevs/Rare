820a856baf42410cfa7e2acb8d7063fc5b060d79
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-11-14 20:40:42 +0200

    ProtonSettings: make compat tool combobox full length

--- rare/components/tabs/settings/widgets/proton.py
+++ rare/components/tabs/settings/widgets/proton.py
@@ -79,7 +79,7 @@ class ProtonSettings(QGroupBox):
         folder_layout.addWidget(self.compat_combo)
         folder_layout.addWidget(self.compat_edit)
         layout.addRow(self.tr("Compat folder"), folder_layout)
-        layout.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.ExpandingFieldsGrow)
+        layout.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.AllNonFixedFieldsGrow)
         layout.setLabelAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
         layout.setFormAlignment(Qt.AlignmentFlag.AlignLeading | Qt.AlignmentFlag.AlignTop)
         # layout.addRow(button_layout)
