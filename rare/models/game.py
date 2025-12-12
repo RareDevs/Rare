5724bb0566f92dbad4dbfb1a28ec371917314d0b
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-12-12 22:27:17 +0200

    RareGame: do not remove .egstore folder by mistake when removing a DLC

--- rare/models/game.py
+++ rare/models/game.py
@@ -310,7 +310,8 @@ class RareGame(RareGameSlim):
         else:
             if self.has_update:
                 self.signals.download.dequeue.emit(self.app_name)
-            self.core.egstore_delete(self.igame)
+            if not self.is_dlc:
+                self.core.egstore_delete(self.igame)
             self.igame = None
             self.signals.game.uninstalled.emit(self.app_name)
         self.__update_pixmap()
