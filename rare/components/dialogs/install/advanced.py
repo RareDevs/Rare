6dd948436a64f28a1685d123ae3a4686659d664f
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2026-08-10 12:09:58 +0300

    InstallDialog: make informative labels follow global style

--- rare/components/dialogs/install/advanced.py
+++ rare/components/dialogs/install/advanced.py
@@ -14,6 +14,16 @@ class InstallDialogAdvanced(CollapsibleFrame):
         self.widget = QWidget(parent=self)
         self.ui = Ui_InstallDialogAdvanced()
         self.ui.setupUi(self.widget)
+
+        self.ui.max_workers_info.setObjectName('InfoLabel')
+        self.ui.max_memory_info.setObjectName('InfoLabel')
+        self.ui.install_prereqs_check.setObjectName('InfoLabel')
+        self.ui.read_files_check.setObjectName('InfoLabel')
+        self.ui.dl_optimizations_check.setObjectName('InfoLabel')
+        self.ui.force_download_check.setObjectName('InfoLabel')
+        self.ui.ignore_space_check.setObjectName('InfoLabel')
+        self.ui.download_only_check.setObjectName('InfoLabel')
+
         self.setWidget(self.widget)
 
 
