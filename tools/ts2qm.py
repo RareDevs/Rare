153938f90b8e9bf572ae964812f8214c6456c44e
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2026-08-15 11:47:04 +0300

    tools: fix ts2qm

--- tools/ts2qm.py
+++ tools/ts2qm.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
-import os
-import shlex
 import subprocess
 from pathlib import Path
 
-basedir = Path(__file__).parent.parent.joinpath("rare/resources/languages/")
+basedir = Path(__file__).parent.parent.joinpath('rare/resources/languages/')
 
 for f in basedir.iterdir():
-    if f.suffix == ".ts" and f.name != "source.ts":
+    if f.suffix == '.ts' and f.name != 'source.ts':
         subprocess.run(
-            shlex.join(
-                (
-                    "pyside6-lrelease",
-                    "-compress",
-                    "-removeidentical",
-                    f.as_posix(),
-                )
-            ),
-            check=False
+            [
+                'pyside6-lrelease',
+                '-compress',
+                '-removeidentical',
+                f.as_posix(),
+            ],
+            check=False,
         )
