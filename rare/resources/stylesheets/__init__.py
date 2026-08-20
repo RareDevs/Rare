8fcead2eda69503b7c5db47a57cefe867eb8ada1
Alessio Attilio <317110166+alessio-attilio@users.noreply.github.com>
2026-08-20 13:53:16 +0200

    Rename RareAccent theme to Cyanotic

--- rare/resources/stylesheets/__init__.py
+++ rare/resources/stylesheets/__init__.py
@@ -1,7 +1,7 @@
 from rare.resources.stylesheets import RareStyle
-from rare.resources.stylesheets import RareAccent
+from rare.resources.stylesheets import Cyanotic
 
 # Incomplete
 # import rare.resources.stylesheets.ChildOfMetropolis as ChildOfMetropolis
 
-__all__ = ['RareStyle', 'RareAccent']
+__all__ = ['RareStyle', 'Cyanotic']
